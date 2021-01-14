c22900063569ae0d5368fa2c41dcf7f31aa181f4
jm33-m0 <jm33.m0@gmail.com>
2021-01-14 21:05:02 +0800

    a rasm2 wrapper to disassemble raw shellcode

---
+++ shellcode/disassemble_raw.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! command -v rasm2 >/dev/null 2>&1; then
+    echo "rasm2 not found, please install radare2 first"
+    exit 1
+fi
+
+if [ -z "$1" ]; then
+    echo "usage: $0 <shellcode.bin>"
+    exit 1
+fi
+
+rasm2 -d -a x86 -b 64 "$(xxd -i "$1" | grep 0x | tr -d ',' | tr -d '[:space:]' | sed 's/0x//g')"
