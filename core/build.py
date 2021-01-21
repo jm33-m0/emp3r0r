5f2ce3154e9b553eada46309e80aaf0c7138c99c
jm33-m0 <jm33.m0@gmail.com>
2021-01-21 22:34:30 +0800

    do not append uuid to cc build

--- core/build.py
+++ core/build.py
@@ -85,7 +85,10 @@ class GoBuild:
         # cmd = f'''GOOS={self.GOOS} GOARCH={self.GOARCH}''' + \
         # f''' go build -ldflags='-s -w -extldflags "-static"' -o ../../build/{self.target}'''
         cmd = f'''GOOS={self.GOOS} GOARCH={self.GOARCH} CGO_ENABLED=0''' + \
-            f''' go build -ldflags='-s -w' -o ../../build/{self.target}-{self.UUID}'''
+            f''' go build -ldflags='-s -w' -o ../../build/{self.target}'''
+        if self.target == "agent":
+            cmd = f'''GOOS={self.GOOS} GOARCH={self.GOARCH} CGO_ENABLED=0''' + \
+                f''' go build -ldflags='-s -w' -o ../../build/{self.target}-{self.UUID}'''
         os.system(cmd)
         log_warn("GO BUILD ends...")
 
