13dfe858b97dde1823a08d5350de0b71f539eac1
jm33-m0 <jm33.m0@gmail.com>
2021-01-25 12:26:34 +0800

    version tracking: test

--- core/submit.sh
+++ core/submit.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+yes_no() {
+    while true; do
+        read -r -p "$* [y/n]: " yn
+        case $yn in
+        [Yy]*) return 0 ;;
+        [Nn]*)
+            echo "Aborted"
+            return 1
+            ;;
+        esac
+    done
+}
+
+yes_no "[?] Add new tag" && (
+    read -r -p "[?] Enter new version tag: " tag
+    git tag "$tag" -a -m "$tag"
+)
+
 version=$(git describe --tags || git describe --always)
 echo "$version" >.version
 git add .version
 git commit .version -m "bump version to $version"
-git push
+git push --tags
