cb93548f9156cd2bc88ba7341ce24b0c8d260bcd
jm33-m0 <jm33.m0@gmail.com>
2021-04-11 15:01:02 +0800

    aes raw encrypt/decrypt

--- core/lib/tun/crypto.go
+++ core/lib/tun/crypto.go
@@ -12,6 +12,54 @@ import (
 	"log"
 )
 
+// AESEncryptRaw encrypt bytes
+func AESEncryptRaw(key, plaintext []byte) []byte {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		log.Print(err)
+		return nil
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return ciphertext
+}
+
+// AESDecryptRaw decrypt bytes
+func AESDecryptRaw(key, ciphertext []byte) []byte {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		log.Print("ciphertext too short")
+		return nil
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return ciphertext
+}
+
 // AESEncrypt string to base64 crypto using AES
 func AESEncrypt(key []byte, text string) string {
 	plaintext := []byte(text)
