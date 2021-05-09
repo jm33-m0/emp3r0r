82f433007fb34b2153e1c9c743d83970786d9451
jm33-m0 <jm33.m0@gmail.com>
2021-05-09 16:26:57 +0800

    run_proxy: stop proxy

--- core/lib/cc/modportfwd.go
+++ core/lib/cc/modportfwd.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"strconv"
 	"time"
+
+	"github.com/jm33-m0/emp3r0r/core/lib/agent"
 )
 
 func modulePortFwd() {
@@ -54,33 +56,33 @@ func modulePortFwd() {
 }
 
 func moduleProxy() {
-	// proxy
-	proxyCtx, proxyCancel := context.WithCancel(context.Background())
 	port := Options["port"].Val
 	status := Options["status"].Val
 
 	// port-fwd
 	var pf PortFwdSession
 	pf.Ctx, pf.Cancel = context.WithCancel(context.Background())
-	pf.Lport, pf.To = port, "127.0.0.1:"+port
-
-	// proxy command, start socks5 server on agent
-	go func() {
-		if _, err := strconv.Atoi(port); err != nil {
-			CliPrintError("Invalid port: %v", err)
-			return
-		}
-		cmd := fmt.Sprintf("!proxy %s %s", status, port)
-		err := SendCmd(cmd, CurrentTarget)
-		if err != nil {
-			CliPrintError("SendCmd: %v", err)
-			return
-		}
-		defer proxyCancel() // mark proxy command as done
-	}()
+	pf.Lport, pf.To = port, "127.0.0.1:"+agent.ProxyPort
 
 	switch status {
 	case "on":
+		// proxy
+		proxyCtx, proxyCancel := context.WithCancel(context.Background())
+		// proxy command, start socks5 server on agent
+		go func() {
+			if _, err := strconv.Atoi(port); err != nil {
+				CliPrintError("Invalid port: %v", err)
+				return
+			}
+			cmd := fmt.Sprintf("!proxy %s %s", status, pf.To)
+			err := SendCmd(cmd, CurrentTarget)
+			if err != nil {
+				CliPrintError("SendCmd: %v", err)
+				return
+			}
+			defer proxyCancel() // mark proxy command as done
+		}()
+
 		// port mapping
 		go func() {
 			for proxyCtx.Err() == nil {
