4216964c6b5a299790c46ef8ba47c02b6e0cb64b
jm33-m0 <jm33@jm33.me>
2022-02-02 17:50:56 +0800

    update built-in bashrc

--- core/lib/data/bash.go
+++ core/lib/data/bash.go
@@ -83,6 +83,7 @@ esac
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
     alias ls='ls --color=auto'
+    alias ip='ip -color=auto'
     alias dir='dir --color=auto'
     alias vdir='vdir --color=auto'
     alias ip='ip --color=auto'
@@ -98,14 +99,4 @@ alias c='clear'
 alias q='exit'
 alias la='ls -A'
 alias l='ls -CF'
-
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
-if ! shopt -oq posix; then
-    if [ -f /usr/share/bash-completion/bash_completion ]; then
-        . /usr/share/bash-completion/bash_completion
-    elif [ -f /etc/bash_completion ]; then
-        . /etc/bash_completion
-    fi
-fi`
+`
