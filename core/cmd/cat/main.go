62bb24e529713bc2fd3bd42c1cee8f35814e0614
jm33-m0 <jm33@jm33.me>
2022-02-21 14:22:41 +0800

    implement my own cat since cat(1) would exit on EOF/Interrupt

---
+++ core/cmd/cat/main.go
@@ -0,0 +1,37 @@
+package main
+
+// This is a small utility that acts like cat(1), see `man 1 cat`
+// Primarily used by emp3r0r's C2 UI
+
+import (
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig)
+
+	go func() {
+		for {
+			s := <-sig
+			// ignore any signals except SIGTERM
+			// (and SIGKILL that cannot be ignored)
+			if s == syscall.SIGTERM {
+				log.Fatal("Terminated")
+			}
+		}
+	}()
+
+	// copies everything from stdin to stdout
+	// if any error occurs, abort
+	for {
+		_, err := io.Copy(os.Stdin, os.Stdout)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
