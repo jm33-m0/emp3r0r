0fb12f45ceceb795232b7f3307acd6fcf89d5296
jm33-m0 <jm33@jm33.me>
2022-03-13 11:03:16 +0800

    list python packages

--- core/modules/py_info/run.py
+++ core/modules/py_info/run.py
@@ -3,10 +3,12 @@
 import sys
 import os
 
-print("Hello, if you see this line, emp3r0r's python is working")
+os.system("python -m pip list")
+
+print("\n\nHello, if you see this line, emp3r0r's python is working")
 print("Let's check some basic info\n")
 print(f"sys.path = {sys.path}")
 print(f"sys.argv = {sys.argv}")
 print(f"sys.api_version = {sys.api_version}")
 print(f"sys.platform= {sys.platform}")
-print(f"ENV = {os.environ}")
+print(f"ENV = {os.environ}\n\n")
