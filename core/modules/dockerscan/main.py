4891f95533b3b5abac8453ba7a2bf022f95c2344
jm33-m0 <jm33@jm33.me>
2022-03-14 20:47:01 +0800

    turn off debug message

--- core/modules/dockerscan/main.py
+++ core/modules/dockerscan/main.py
@@ -6,7 +6,7 @@ libs = f"{os.getcwd()}/libs"
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(1, parent_dir)
 sys.path.insert(1, libs)
-print(sys.path)
+# print(sys.path)
 
 import dockerscan
 
