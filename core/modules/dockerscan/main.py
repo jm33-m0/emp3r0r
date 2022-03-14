71248f25863e566295bb8e0d84d7a6b1da2a4d78
jm33-m0 <jm33@jm33.me>
2022-03-14 21:14:30 +0800

    dockerscan: fix argument passing

--- core/modules/dockerscan/main.py
+++ core/modules/dockerscan/main.py
@@ -3,6 +3,10 @@
 import os
 import sys
 
+if len(sys.argv) > 1:
+    args = os.environ["ARGS"]
+    os.system(f"{sys.executable} {sys.argv[0]} {args}")
+
 libs = f"{os.getcwd()}/libs"
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
