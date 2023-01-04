72c4cea4bf2d6dc178b55531c58f6f632a717765
jm33-m0 <jm33@jm33.me>
2023-01-04 12:48:03 +0800

    feature: use AES-CBC mode to support tiny-AES

---
+++ core/lib/tun/aes.go
@@ -0,0 +1,154 @@
+package tun
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"log"
+)
+
+// Go implementation of PKCS5Padding
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS5Trimming(encrypt []byte) []byte {
+	padding := encrypt[len(encrypt)-1]
+	return encrypt[:len(encrypt)-int(padding)]
+}
+
+// AESEncryptRaw encrypt bytes
+func AESEncryptRaw(key, plaintext []byte) []byte {
+	if len(plaintext)%aes.BlockSize != 0 {
+		plaintext = PKCS5Padding(plaintext, aes.BlockSize)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		log.Print(err)
+		return nil
+	}
+
+	cbc := cipher.NewCBCEncrypter(block, iv)
+	cbc.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
+
+	return ciphertext
+}
+
+// AESDecryptRaw decrypt bytes
+func AESDecryptRaw(key, ciphertext []byte) []byte {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		log.Print("ciphertext too short")
+		return nil
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	cbc := cipher.NewCBCDecrypter(block, iv)
+
+	cbc.CryptBlocks(ciphertext, ciphertext)
+
+	return PKCS5Trimming(ciphertext)
+}
+
+// AESEncrypt string to base64 crypto using AES
+func AESEncrypt(key []byte, text string) string {
+	plaintext := []byte(text)
+	if len(text)%aes.BlockSize != 0 {
+		plaintext = PKCS5Padding([]byte(text), aes.BlockSize)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Print(err)
+		return ""
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		log.Print(err)
+		return ""
+	}
+
+	cbc := cipher.NewCBCEncrypter(block, iv)
+	cbc.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
+
+	// convert to base64
+	return base64.URLEncoding.EncodeToString(ciphertext)
+}
+
+// AESDecrypt from base64 to decrypted string
+func AESDecrypt(key []byte, cryptoText string) string {
+	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Print(err)
+		return ""
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		log.Print("ciphertext too short")
+		return ""
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	cbc := cipher.NewCBCDecrypter(block, iv)
+
+	cbc.CryptBlocks(ciphertext, ciphertext)
+
+	return fmt.Sprintf("%s", PKCS5Trimming(ciphertext))
+}
+
+// GenAESKey generate AES key from any string
+func GenAESKey(seed string) []byte {
+	md5sum := MD5Sum(seed)
+	return []byte(md5sum)[:32]
+}
+
+// func TestAES() {
+// 	plaintext := "123456"
+// 	plainbytes := []byte(plaintext)
+//
+// 	key := GenAESKey("adodnodnvpddpmdspcdpsmdpcmspdcmsp")
+//
+// 	cipherText := AESEncrypt(key, plaintext)
+// 	cipherBytes := AESEncryptRaw(key, plainbytes)
+//
+// 	decText := AESDecrypt(key, cipherText)
+// 	decBytes := AESDecryptRaw(key, cipherBytes)
+//
+// 	log.Printf("%s -> %v -> %s",
+// 		plaintext, cipherText, decText)
+// 	log.Printf("%s -> %v -> %s",
+// 		plainbytes, cipherBytes, decBytes)
+// }
--- core/lib/tun/crypto.go
+++ core/lib/tun/crypto.go
@@ -1,10 +1,7 @@
 package tun
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
 	"crypto/md5"
-	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
@@ -12,113 +9,6 @@ import (
 	"log"
 	"os"
 )
-
-// AESEncryptRaw encrypt bytes
-func AESEncryptRaw(key, plaintext []byte) []byte {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		log.Print(err)
-		return nil
-	}
-
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
-	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		log.Print(err)
-		return nil
-	}
-
-	stream := cipher.NewCFBEncrypter(block, iv)
-	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
-
-	return ciphertext
-}
-
-// AESDecryptRaw decrypt bytes
-func AESDecryptRaw(key, ciphertext []byte) []byte {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		log.Print(err)
-		return nil
-	}
-
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
-	if len(ciphertext) < aes.BlockSize {
-		log.Print("ciphertext too short")
-		return nil
-	}
-	iv := ciphertext[:aes.BlockSize]
-	ciphertext = ciphertext[aes.BlockSize:]
-
-	stream := cipher.NewCFBDecrypter(block, iv)
-
-	// XORKeyStream can work in-place if the two arguments are the same.
-	stream.XORKeyStream(ciphertext, ciphertext)
-
-	return ciphertext
-}
-
-// AESEncrypt string to base64 crypto using AES
-func AESEncrypt(key []byte, text string) string {
-	plaintext := []byte(text)
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		log.Print(err)
-		return ""
-	}
-
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
-	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		log.Print(err)
-		return ""
-	}
-
-	stream := cipher.NewCFBEncrypter(block, iv)
-	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
-
-	// convert to base64
-	return base64.URLEncoding.EncodeToString(ciphertext)
-}
-
-// AESDecrypt from base64 to decrypted string
-func AESDecrypt(key []byte, cryptoText string) string {
-	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		log.Print(err)
-		return ""
-	}
-
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
-	if len(ciphertext) < aes.BlockSize {
-		log.Print("ciphertext too short")
-		return ""
-	}
-	iv := ciphertext[:aes.BlockSize]
-	ciphertext = ciphertext[aes.BlockSize:]
-
-	stream := cipher.NewCFBDecrypter(block, iv)
-
-	// XORKeyStream can work in-place if the two arguments are the same.
-	stream.XORKeyStream(ciphertext, ciphertext)
-
-	return fmt.Sprintf("%s", ciphertext)
-}
-
-// GenAESKey generate AES key from any string
-func GenAESKey(seed string) []byte {
-	md5sum := MD5Sum(seed)
-	return []byte(md5sum)[:32]
-}
 
 // MD5Sum calc md5 of a string
 func MD5Sum(text string) string {
