58ed35a412a6357d949963333ae332584d871ea1
jm33-m0 <jm33@jm33.me>
2023-03-30 17:54:03 +0800

    fix: unable to read mac addr in kvm machines (virtio NIC)

--- core/lib/util/sysinfo.go
+++ core/lib/util/sysinfo.go
@@ -87,11 +87,6 @@ func macUint64() uint64 {
 	for _, i := range interfaces {
 		if i.Flags&net.FlagUp != 0 && bytes.Compare(i.HardwareAddr, nil) != 0 {
 
-			// Skip locally administered addresses
-			if i.HardwareAddr[0]&2 == 2 {
-				continue
-			}
-
 			var mac uint64
 			for j, b := range i.HardwareAddr {
 				if j >= 8 {
