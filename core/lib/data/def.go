3a1d82cb768647aea9bdf0988fa04d2f4bc98345
jm33-m0 <jm33@jm33.me>
2023-06-26 16:25:17 +0900

    discourage `all` in `get_persistence`

--- core/lib/data/def.go
+++ core/lib/data/def.go
@@ -122,7 +122,7 @@ var ModuleComments = map[string]string{
 // help module shows this
 var ModuleHelp = map[string]map[string]string{
 	ModPERSISTENCE: {
-		"method": "Method to get persistence, or all of them at once. Available methods: " + PersistMethods["profiles"] + ", " + PersistMethods["injector"] + ", " + PersistMethods["cron"] + ", " + PersistMethods["patcher"],
+		"method": "Method to get persistence, or all of them at once (not recommended). Available methods: " + PersistMethods["profiles"] + ", " + PersistMethods["injector"] + ", " + PersistMethods["cron"] + ", " + PersistMethods["patcher"],
 	},
 	ModCMD_EXEC: {
 		"cmd_to_exec": "Press TAB for some hints",
