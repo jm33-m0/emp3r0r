5c6569653e7e4068528bf31f9bce863e160fbf1d
jm33-m0 <jm33@jm33.me>
2024-12-26 22:52:04 +1100

    fmt

--- loader/elf/tinfzlib.c
+++ loader/elf/tinfzlib.c
@@ -25,71 +25,67 @@
 
 #include "tinf.h"
 
-static unsigned int read_be32(const unsigned char *p)
-{
-	return ((unsigned int) p[0] << 24)
-	     | ((unsigned int) p[1] << 16)
-	     | ((unsigned int) p[2] << 8)
-	     | ((unsigned int) p[3]);
+static unsigned int read_be32(const unsigned char *p) {
+  return ((unsigned int)p[0] << 24) | ((unsigned int)p[1] << 16) |
+         ((unsigned int)p[2] << 8) | ((unsigned int)p[3]);
 }
 
-int tinf_zlib_uncompress(void *dest, unsigned int *destLen,
-                         const void *source, unsigned int sourceLen)
-{
-	const unsigned char *src = (const unsigned char *) source;
-	unsigned char *dst = (unsigned char *) dest;
-	unsigned int a32;
-	int res;
-	unsigned char cmf, flg;
+int tinf_zlib_uncompress(void *dest, unsigned int *destLen, const void *source,
+                         unsigned int sourceLen) {
+  const unsigned char *src = (const unsigned char *)source;
+  unsigned char *dst = (unsigned char *)dest;
+  unsigned int a32;
+  int res;
+  unsigned char cmf, flg;
 
-	/* -- Check header -- */
+  /* -- Check header -- */
 
-	/* Check room for at least 2 byte header and 4 byte trailer */
-	if (sourceLen < 6) {
-		return TINF_DATA_ERROR;
-	}
+  /* Check room for at least 2 byte header and 4 byte trailer */
+  if (sourceLen < 6) {
+    return TINF_DATA_ERROR;
+  }
 
-	/* Get header bytes */
-	cmf = src[0];
-	flg = src[1];
+  /* Get header bytes */
+  cmf = src[0];
+  flg = src[1];
 
-	/* Check checksum */
-	if ((256 * cmf + flg) % 31) {
-		return TINF_DATA_ERROR;
-	}
+  /* Check checksum */
+  if ((256 * cmf + flg) % 31) {
+    return TINF_DATA_ERROR;
+  }
 
-	/* Check method is deflate */
-	if ((cmf & 0x0F) != 8) {
-		return TINF_DATA_ERROR;
-	}
+  /* Check method is deflate */
+  if ((cmf & 0x0F) != 8) {
+    return TINF_DATA_ERROR;
+  }
 
-	/* Check window size is valid */
-	if ((cmf >> 4) > 7) {
-		return TINF_DATA_ERROR;
-	}
+  /* Check window size is valid */
+  if ((cmf >> 4) > 7) {
+    return TINF_DATA_ERROR;
+  }
 
-	/* Check there is no preset dictionary */
-	if (flg & 0x20) {
-		return TINF_DATA_ERROR;
-	}
+  /* Check there is no preset dictionary */
+  if (flg & 0x20) {
+    return TINF_DATA_ERROR;
+  }
 
-	/* -- Get Adler-32 checksum of original data -- */
+  /* -- Get Adler-32 checksum of original data -- */
 
-	a32 = read_be32(&src[sourceLen - 4]);
+  a32 = read_be32(&src[sourceLen - 4]);
 
-	/* -- Decompress data -- */
+  /* -- Decompress data -- */
 
-	res = tinf_uncompress(dst, destLen, src + 2, sourceLen - 6);
+  res = tinf_uncompress(dst, destLen, src + 2, sourceLen - 6);
 
-	if (res != TINF_OK) {
-		return TINF_DATA_ERROR;
-	}
+  if (res != TINF_OK) {
+    return TINF_DATA_ERROR;
+  }
 
-	/* -- Check Adler-32 checksum -- */
+  /* -- Check Adler-32 checksum -- */
 
-	if (a32 != tinf_adler32(dst, *destLen)) {
-		return TINF_DATA_ERROR;
-	}
+  if (a32 != tinf_adler32(dst, *destLen)) {
+    return TINF_DATA_ERROR;
+  }
 
-	return TINF_OK;
+  return TINF_OK;
 }
