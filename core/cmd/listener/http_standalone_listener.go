a480f306611afdaf10f114a84da4909c7f4e81e2
jm33-m0 <jm33@jm33.me>
2024-12-28 17:05:32 +1100

    fix: add plain HTTP listener

--- core/cmd/listener/http_standalone_listener.go
+++ core/cmd/listener/http_standalone_listener.go
@@ -11,11 +11,16 @@ func main() {
 	stagerPath := flag.String("stager", "", "path to the stager file to serve")
 	port := flag.String("port", "8080", "port to serve the stager file on")
 	keyStr := flag.String("key", "my_secret_key", "key to encrypt the stager file")
+	plain_http := flag.Bool("plain", false, "serve stager file over plain HTTP")
 	flag.Parse()
 
 	if *stagerPath == "" {
 		log.Fatal("stager file path is required")
 	}
 
+	if *plain_http {
+		listener.HTTPBareListener(*stagerPath, *port)
+		return
+	}
 	listener.HTTPAESCompressedListener(*stagerPath, *port, *keyStr, true)
 }
