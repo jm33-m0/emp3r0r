88202b6f126ae4398660e8dc3793b7db32e2a7ac
jm33-m0 <jm33@jm33.me>
2025-01-13 12:56:09 +1100

    print success message

--- core/modules/py_info/run.py
+++ core/modules/py_info/run.py
@@ -5,10 +5,10 @@ import os
 
 os.system("python -m pip list")
 
-print("\n\nHello, if you see this line, emp3r0r's python is working")
 print("Let's check some basic info\n")
 print(f"sys.path = {sys.path}")
 print(f"sys.argv = {sys.argv}")
 print(f"sys.api_version = {sys.api_version}")
 print(f"sys.platform= {sys.platform}")
 print(f"ENV = {os.environ}\n\n")
+print("\n\nHello, if you see this line, emp3r0r's python is working")
