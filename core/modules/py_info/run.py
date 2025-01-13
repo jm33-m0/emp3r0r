a0e1cba4d288931d563e65b48d32999845e04fa1
jm33-m0 <jm33@jm33.me>
2025-01-13 13:29:30 +1100

    logging

--- core/modules/py_info/run.py
+++ core/modules/py_info/run.py
@@ -3,12 +3,11 @@
 import sys
 import os
 
+print("\n\nLet's check some basic info")
 os.system("python -m pip list")
-
-print("Let's check some basic info\n")
 print(f"sys.path = {sys.path}")
 print(f"sys.argv = {sys.argv}")
 print(f"sys.api_version = {sys.api_version}")
 print(f"sys.platform= {sys.platform}")
-print(f"ENV = {os.environ}\n\n")
-print("\n\nHello, if you see this line, emp3r0r's python is working")
+print(f"ENV = {os.environ}")
+print("Hello, if you see this line, emp3r0r's python is working")
