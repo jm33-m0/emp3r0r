c76b35b8556c8027bcd9e1a8f9fa981c7073df98
jm33-m0 <jm33@jm33.me>
2026-02-10 22:48:47 +0000

    fix: agent should sleep instead of exiting when `--stager` is not given

--- core/cmd/agent/cond_c2_linux.go
+++ core/cmd/agent/cond_c2_linux.go
@@ -5,10 +5,21 @@ package main
 
 import (
 	"os"
+
+	"github.com/jm33-m0/emp3r0r/core/internal/agent/base/common"
+	"github.com/jm33-m0/emp3r0r/core/lib/logging"
+	"github.com/jm33-m0/emp3r0r/core/lib/util"
 )
 
 // conditionalC2FailNotify tells the parent (stager/loader) to recycle us.
 func conditionalC2FailNotify() {
-	// Exit cleanly (Stager will encrypt and sleep us, then restart)
-	os.Exit(0)
+	// If run by stager, exit cleanly (Stager will encrypt and sleep us, then restart)
+	if common.RuntimeConfig.IsRunByStager {
+		logging.Warningf("Agent lifecycle ended, recycling")
+		os.Exit(0)
+	}
+
+	// Otherwise, just wait and retry
+	logging.Warningf("Connection failed, sleeping...")
+	util.TakeASnap(false) // sleeps for random interval
 }
