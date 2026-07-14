d5c86913000d0dbcf78ad72a9d8a7bcf063136f4
jm33-m0 <jm33@jm33.me>
2026-07-14 15:34:20 +1000

    fix: high CPU usage on non-Linux platforms
    sleep is not implemented

--- core/cmd/agent/cond_c2_other.go
+++ core/cmd/agent/cond_c2_other.go
@@ -3,5 +3,14 @@
 
 package main
 
-// conditionalC2FailNotify is a no-op on non-Linux platforms.
-func conditionalC2FailNotify() {}
+import (
+	"github.com/jm33-m0/emp3r0r/core/lib/logging"
+	"github.com/jm33-m0/emp3r0r/core/lib/util"
+)
+
+// conditionalC2FailNotify is a sleep on non-Linux platforms.
+func conditionalC2FailNotify() {
+	// Wait and retry
+	logging.Warningf("Connection failed, sleeping...")
+	util.TakeASnap() // sleeps for random interval
+}
