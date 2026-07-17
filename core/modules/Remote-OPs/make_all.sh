4e5b6522b91f38c74dfa7ddff793a2707e9814a2
jm33-m0 <jm33@jm33.me>
2026-07-17 12:47:02 +1000

    place object files in current dir

--- core/modules/Remote-OPs/make_all.sh
+++ core/modules/Remote-OPs/make_all.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Remove mkdir and mv lines from all Makefiles to keep object files in their source directories
-find src -name Makefile -exec sed -i '/mkdir -p/d; /mv $(BOFNAME)\*\.o/d; /mv .* \.\.\//d' {} +
-
 cd src/Remote
 ls | while read dir; do
     if [[ -d $dir ]]; then
